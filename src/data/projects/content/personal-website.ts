a774aa2467b1c1b6fd2c102647469923f8520de1
Sean McHale <seanryanmchale@yahoo.com>
2024-10-30 20:01:18 +0800

    minor wording changes

--- src/data/projects/content/personal-website.ts
+++ src/data/projects/content/personal-website.ts
@@ -1,6 +1,3 @@
 export const description = `
 Want to learn about how this website works under the hood?
-Though it's a relatively simple project, a good amount of thought and effort went into its creation.
-
-
 `;
